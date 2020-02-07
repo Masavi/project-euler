36567afda21352aa3a6198a9efb3f8c1b447ef5d
maui <maui@devf.mx>
2020-02-07 05:19:39 -0600

    add 001 solution

--- 001-multiples-of-3-and-5/main.kt
+++ 001-multiples-of-3-and-5/main.kt
@@ -8,13 +8,34 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 ------------------------------------------------------ */
 
-// 1) is it a multiple of 3 or 5?
-fun isMultiple(args: Array<Int>) {    
-  // args.forEach { println(it) }
-  println(args[0]);
+// 1) which numbers are multiples of 3 or 5?
+fun getMultiples3or5(limit: Int): Set<Int> {
+  var multiples = setOf<Int>()
+
+  for (i in 1..(limit - 1)) {
+    if(i%3 == 0 || i%5 == 0) {
+      // val message = "Hello world $i" // String and Int concatenation
+      // println(message);
+      multiples += i;
+    }
+  }
+
+  val numberOfMultiples = multiples.size;
+  println("number of multiples found:\n$numberOfMultiples");
+
+  return multiples;
+}
+
+// 2) what is the sum of all the multiples?
+fun getSumOfNumbers(numbers: Set<Int>) :Int {
+  var sum = 0;
+  numbers.forEach { sum += it };
+  return sum;
 }
 
 fun main(args: Array<String>) {
-  val numbers = arrayOf(3,5); // [3, 5]
-  isMultiple(numbers); 
+  val limit = 1000;
+  val multiples = getMultiples3or5(limit);
+  val result = getSumOfNumbers(multiples);
+  println("sum of all the multiples of 3 or 5 below $limit:\n$result");
 }
