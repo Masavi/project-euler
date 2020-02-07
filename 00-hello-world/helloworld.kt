80e370e5047bad0bfc3f650344f2909701a7c86a
maui <maui@devf.mx>
2020-02-07 03:53:53 -0600

    learning kotlin

---
+++ 00-hello-world/helloworld.kt
@@ -0,0 +1,13 @@
+fun pretty(text: String){
+    println("\n---------------");
+    println(text);
+    println("---------------");
+}
+
+fun main(args: Array<String>){
+  pretty("🌎 hola mundo 🌎");
+}
+
+// fun foo(): Unit {
+
+// }
